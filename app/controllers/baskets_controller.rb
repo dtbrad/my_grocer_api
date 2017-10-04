08627d05ac3d6a574f65fb4b5f3e088899aafbdc
Daniel Brad <danieltbrad@gmail.com>
2017-10-03 21:01:10 -0700

    Add date range limiter to baskets

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,6 +1,6 @@
 class BasketsController < ApplicationController
   def index
-    baskets = Basket.all
+    baskets = Basket.within_date_range(params)
     paginate json: baskets
   end
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,4 +2,12 @@ class Basket < ApplicationRecord
   belongs_to :user, optional: true
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+
+  def self.within_date_range(args={})
+    beginning = args.fetch(:beginning, '2014-01-01')
+    finish = args.fetch(:finish, '2017-10-01')
+    start_date = DateTime.parse(beginning)
+    end_date = DateTime.parse(finish)
+    Basket.where(transaction_date: start_date..end_date)
+  end
 end
