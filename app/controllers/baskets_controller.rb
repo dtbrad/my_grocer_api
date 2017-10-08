21505760cd0d54be1181456c4ec22d28e01a7b51
Daniel Brad <danieltbrad@gmail.com>
2017-10-08 10:24:24 -0700

    Refactor baskets#index

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,11 +1,13 @@
 class BasketsController < ApplicationController
 
   def index
-    baskets = Basket.within_date_range(params)
-    first_date = baskets.last.transaction_date
-    last_date = baskets.first.transaction_date
-    baskets_array = paginate Basket.within_date_range(params)
-    render json: { baskets_array: { b: baskets_array, each_serializer: BasketSerializer }, first_date: first_date, last_date: last_date }
+    ordered_baskets = Basket.within_date_range(params)
+    custom_sorted_baskets = ordered_baskets.custom_sort(params[:category], params[:direction])
+    oldest_date = ordered_baskets.last.transaction_date
+    newest_date = ordered_baskets.first.transaction_date
+    baskets_array = paginate custom_sorted_baskets
+    chart_array = Basket.group_baskets(params)
+    render json: { baskets_array: { b: baskets_array, each_serializer: BasketSerializer }, chart_array: chart_array, oldest_date: oldest_date, newest_date: newest_date, dir: params[:direction] }
   end
 
   def spending_history
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -3,19 +3,43 @@ class Basket < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
 
+
+  def self.custom_sort(category, direction)
+    category = category || "sort_date"
+    direction = direction || "desc"
+    direction = 'asc'.casecmp(direction).zero? ? 'asc' : 'desc'
+    send(category, direction)
+  end
+
+  def self.sort_date(direction)
+    order = ["baskets.transaction_date", direction].join(" ")
+    order(order)
+  end
+
+  def self.sort_items(direction)
+    order = ["baskets.line_item_count", direction].join(" ")
+    order(order)
+  end
+
+  def self.sort_total(direction)
+    order = ["baskets.total_cents", direction].join(" ")
+    order(order)
+  end
+
+
   def self.within_date_range(args={})
-    beginning = args.fetch(:beginning, '2015-11-23')
-    finish = args.fetch(:finish, '2017-09-27')
-    start_date = DateTime.parse(beginning)
-    end_date = DateTime.parse(finish)
-    Basket.where(transaction_date: start_date..end_date)
+    oldest_date = args.fetch(:oldest_date, '2015-11-23')
+    newest_date = args.fetch(:newest_date, '2017-09-27')
+    start_date = DateTime.parse(oldest_date)
+    end_date = DateTime.parse(newest_date)
+    where(transaction_date: start_date..end_date)
   end
 
   def self.group_baskets(args={})
-    beginning = args.fetch(:beginning, '2015-11-23')
-    finish = args.fetch(:finish, '2017-10-01')
-    start_date = DateTime.parse(beginning)
-    end_date = DateTime.parse(finish)
+    oldest_date = args.fetch(:oldest_date, '2015-11-23')
+    newest_date = args.fetch(:newest_date, '2017-09-27')
+    start_date = DateTime.parse(oldest_date)
+    end_date = DateTime.parse(newest_date)
     unit = args.fetch(:unit, Basket.pick_unit(start_date, end_date))
     data = group_by_period(unit, :transaction_date, range: start_date..end_date).sum('baskets.total_cents').to_a
     {data: data, unit: unit}
